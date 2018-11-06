57646a5ee24ed30e4629a66a2b820dce4684ddbb
James Lotruglio <jimlotruglio@yahoo.com>
2018-11-06 23:42:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const driver ={}
 
 function updateDriverWithKeyAndValue(driver, key, value){
-  Object.assign({}, driver, name "Sam")
+  Object.assign({}, driver, name: "Sam", address: '11 Broadway')
   driver.address = '11 Broadway'
   return driver 
 }
